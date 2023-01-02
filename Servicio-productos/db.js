ec75b22778225b7399b854e66d6a09ff654d5192
anangelicacyz <anangeluxd@gmail.com>
2023-01-02 17:08:04 -0600

    endpoint servicio usuarios

---
+++ Servicio-productos/db.js
@@ -0,0 +1,13 @@
+let db = {
+    productos: [
+        {id: 1, name: "huevo", price: 10, units: 500},
+        {id: 2, name: "leche", price: 25, units: 200}
+    ],
+    usuarios: [
+        {id: 1, name: "david", lastname: "zl", email: "david@gmail.com"},
+        {id: 2, name: "anita", lastname: "cyz", email: "anita@gmail.com"},
+        {id: 3, name: "agro", lastname: "ccd", email: "agro@gmail.com"}
+    ]
+    }
+//exportar un objeto a otro archivo
+    module.exports = db
--- Servicio-productos/server.js
+++ Servicio-productos/server.js
@@ -1,26 +1,22 @@
 // importar libreria:toda la libreria que se llama express
 //se metió en la variable const express
 const express = require("express")
+const db = require("./db")
 /* ejemplo
 const ana = require("ana")*/
 
 //inicializamos express dentro de 
 //variable app 
 const app = express()
+app.use(express.json())
 //request, response
 
 //endpoint GET /productos
-// id name   price  units
-// 1  huevo    10    500
-// 2  leche    25    200
-let productos = [
 
-    {id: 1, name: "huevo", price: 10, units: 500},
-    {id: 2, name: "leche", price: 25, units: 200}
-]
+
 app.get("/productos", (req, res)=>{
 
-    res.json(productos)
+    res.json(db.productos)
 
 })
 //endpoint buscar por id
@@ -28,7 +24,7 @@ app.get("/productos/:id", (req, res)=>{
     //obtener variable id
     const idProduct = req.params.id
     
-    const resultadoBusqueda = productos.find(producto => producto.id === Number(idProduct))
+    const resultadoBusqueda = db.productos.find(producto => producto.id === Number(idProduct))
     res.json(resultadoBusqueda)
 
 })
@@ -37,7 +33,7 @@ app.delete("/productos/:id", (req, res)=>{
     //obtener variable id
     const idProduct = req.params.id
     
-    const validationProduct = productos.find(producto => producto.id === Number(idProduct))
+    const validationProduct = db.productos.find(producto => producto.id === Number(idProduct))
 
     if(!validationProduct){
             res.status(404).json({
@@ -46,10 +42,107 @@ app.delete("/productos/:id", (req, res)=>{
             return
         }
 
-    productos = productos.filter(producto => producto.id !== validationProduct.id)
-    res.json(productos)
+    db.productos = db.productos.filter(producto => producto.id !== validationProduct.id)
+    res.json(db.productos)
+
+})
+app.post("/productos", (req, res)=>{
+    const name= req.body.name
+    const price=req.body.price
+    const units= req.body.units || 0
+        if(!name || !price){
+            res.status(400).json({
+                message: "Los campos Name y Price  son obligatorios"
+            })
+        }
+    const id = db.productos.length + 1 
+
+    let productoNuevo = {
+        id: id , name: name , price: price, units: units 
+    }
+
+    db.productos.push(productoNuevo)
+    res.json(db.productos)
+}
+)
+app.put("/productos/:idProducto" , (req, res)=>{
+    const idProducto= Number(req.params.idProducto)
+    const indice = db.productos.findIndex(producto => producto.id === idProducto)
+    if(indice<0){
+        res.status(404).json(
+            {
+                message : "No existe elemento para actualizar"
+            }
+        )
+    }
+    const name= req.body.name
+    const price= req.body.price
+    const units = req.body.units
+
+    let productoActualizado = {
+        name,  price, units, id: idProducto
+    }
+    db.productos [indice] =productoActualizado
+    res.json( db.productos)
 
 })
 app.listen(9000, ()=>{
     console.log("corriendo puerto 9000")
 } )
+app.get("/usuarios", (req,res)=>{
+    res.json(db.usuarios)
+})
+app.get("/usuarios/:idUser", (req, res) =>{
+    const idUsuario= Number(req.params.idUser)
+
+    const ResultadoUsuario = db.usuarios.find(usuario => usuario.id === idUsuario)
+    res.json(ResultadoUsuario)
+})
+app.delete("/usuarios/:idUser", (req, res) =>{
+    const idUser = Number(req.params.idUser)
+    ValidationUser = db.usuarios.find(usuario => usuario.id === idUser)
+        if(!ValidationUser){
+            res.status(404).json({
+                message: "ID not found"
+            })
+        }
+    db.usuarios = db.usuarios.filter(usuario => usuario.id !== ValidationUser.id)
+    res.json(db.usuarios)
+})
+app.post("/usuarios", (req, res) =>{
+    const name= req.body.name
+    const lastname= req.body.lastname
+    const email= req.body.email
+   
+      
+        if(!name || !lastname || !email){
+            res.status(400).json({
+                message: "Todos los campos son requeridos"
+            })
+        }
+        const id= db.usuarios.length + 1
+    let usuarioNuevo = {
+        id, name, lastname, email
+    }
+    db.usuarios.push(usuarioNuevo)
+    res.json(db.usuarios)
+})
+app.put("/usuarios/:idUser", (req, res) =>{
+    const idUser= Number(req.params.idUser)
+    const index= db.usuarios.findIndex(usuario => usuario.id === idUser)
+
+    if(index <0){
+        res.status(404).json({
+            message: "No hay datos para actualizar"
+        })
+    }
+const name= req.body.name
+const lastname= req.body.lastname
+const email= req.body.email
+
+    let actualizacionUsuario = {
+        name, lastname, email, id : idUser
+    }
+    db.usuarios[index]= actualizacionUsuario
+    res.json(db.usuarios)
+})
